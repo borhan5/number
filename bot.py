6f7e113527ffbab796825c083745dc58dd152c5b
borhan5 <65207546+borhan5@users.noreply.github.com>
2026-06-26 10:09:55 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from telebot import types
 from waitress import serve
 
 # --- কনফিগারেশন ---
-BOT_TOKEN = '8953289994:AAEAO-JUoARj2RJRkOoi7BHuXqbHwjfvahw'
+BOT_TOKEN = '8953289994:AAHTHGnduASu43Q5fX4TOXq_ajXEDaNC6f8'
 API_KEY = 'MQGVM5B5OOW'
 BASE_URL = 'https://api.2oo9.cloud/MXS47FLFX0U/tnevs/@public/api'
 
